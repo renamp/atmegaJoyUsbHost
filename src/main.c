c12749e2f01e14b9675ac5e766b964bdef1365ea
renamp <renamp2006@gmail.com>
2025-12-20 23:57:40 +0100

    Add main
    Create main file

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * atmegaJoyUsbHost.c
+ *
+ * Created: 12/20/2025 11:29:42 PM
+ *  Author: Renan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
